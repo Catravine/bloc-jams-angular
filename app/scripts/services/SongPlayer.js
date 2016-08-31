e55083f4e6c5b0657f9ecc6e72cc261bd296723f
Caroline Courtney <caroline@carolinecourtney.com>
2016-08-31 17:49:06 -0400

    Assignment 8: Services Part 3: completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -26,8 +26,7 @@
     */
     var setSong = function(song) {
       if (currentBuzzObject) {
-        currentBuzzObject.stop();
-        SongPlayer.currentSong.playing = null;
+        stopSong();
       }
 
       currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -46,6 +45,15 @@
     var playSong = function(song) {
       currentBuzzObject.play();
       song.playing = true;
+    }
+
+    /**
+    * @function stopSong
+    * @desc stops playback and sets the currently playing song to 'null'.
+    */
+    var stopSong = function() {
+      currentBuzzObject.stop();
+      SongPlayer.currentSong.playing = null;
     }
 
     /**
@@ -94,14 +102,29 @@
 
     /**
     * @function previous
-    * @desc decrements the current song index (based on album)
+    * @desc decrements the current song index (based on album) and plays previous track
     */
     SongPlayer.previous = function() {
       var currentSongIndex = getSongIndex(SongPlayer.currentSong);
       currentSongIndex --;
       if (currentSongIndex < 0) {
-        currentBuzzObject.stop();
-        SongPlayer.currentSong.playing = null;
+        stopSong();
+      } else {
+        var song = currentAlbum.songs[currentSongIndex];
+        setSong(song);
+        playSong(song);
+      }
+    };
+
+    /**
+    * @function next
+    * @desc increments the current song index (based on album) and plays next track
+    */
+    SongPlayer.next = function() {
+      var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+      currentSongIndex ++;
+      if (currentSongIndex >= currentAlbum.songs.length) {
+        stopSong();
       } else {
         var song = currentAlbum.songs[currentSongIndex];
         setSong(song);
