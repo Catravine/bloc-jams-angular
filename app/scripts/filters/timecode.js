5de0e3c827aa3a7fdd938088ff449545cb63fecb
Caroline Courtney <caroline@carolinecourtney.com>
2016-09-05 13:53:17 -0400

    Assignment 11 Filters: completed '

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -8,17 +8,7 @@
       }
 
       var wholeSeconds = Math.floor(seconds);
-      var minutes = Math.floor(wholeSeconds / 60);
-      var remainingSeconds = wholeSeconds % 60;
-
-      var output = minutes + ':';
-
-      if (remainingSeconds < 10) {
-        output += '0';
-      }
-
-      output += remainingSeconds;
-      return output;
+      return buzz.toTimer(wholeSeconds);
     };
   }
 
